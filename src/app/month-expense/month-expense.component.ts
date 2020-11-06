b28b963192d57d9517d1717c0135bb3c22a9f80f
mikepruebas23 <mikepruebas23@gmail.com>
2020-11-06 13:54:27 -0700

    estable cards

--- src/app/month-expense/month-expense.component.ts
+++ src/app/month-expense/month-expense.component.ts
@@ -9,28 +9,29 @@ export class MonthExpenseComponent implements OnInit {
 
   constructor() { }
 
-  persons = [
+  beneficios = [
     {
-      title:"Diciembre",
-      income: 200,
-      expenses: 100 
+      title:"Siempre cerca de ti",
+      description:"En nuestros más de 3,000 módulos de atención ubicados en las más de 1,500 tiendas Coppel y Zapatería Coppel Canadá de todo el país, a su servicio en el horario de las tiendas incluyendo sábados y domingos.",
     },
     {
-      title:"Enero",
-      income: 500,
-      expenses: 700 
+      title:"Seguridad y confianza",
+      description:"Los recursos de su cuenta individual son administrados por personas expertas en inversiones.",
     },
     {
-      title:"Diciembre",
-      income: 200,
-      expenses: 100 
+      title:"Asesoría personalizada",
+      description:"Lo atendemos con la sencillez y amabilidad que usted conoce, brindándole información y asesoría para que obtenga todos los beneficios del sistema de pensiones.",
     },
     {
-      title:"Enero",
-      income: 500,
-      expenses: 700 
+      title:"Transparencia",
+      description:"Puede consultar su saldo en cualquier momento en nuestros módulos de atención o en nuestra página de Internet, además del envío a su domicilio de 3 estados de cuenta al año.",
     }
   ]
   ngOnInit(): void {
   }
+
+  si(){
+    alert('si');
   }
+  }
+
